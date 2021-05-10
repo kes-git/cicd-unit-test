e0e7e9907c93111fe86d7bb1719e9eb048282b80
Laksiri Fernando <laksirif@kingslake.com>
2021-05-11 00:47:35 +0530

    fail the test to see the result

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,6 @@ const app = require('../index');
 describe('Simple Math Test', () => {
     it('should return 5', () => {
         const result = app.add(2, 3);
-        expect(result).to.equal(5);
+        expect(result).to.equal(6);
     });
 });
